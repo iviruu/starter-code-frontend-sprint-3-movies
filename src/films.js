a29a401b6ef99bffb7bd37763975889295e56d51
iviruu <ivan.kok.ru@gmail.com>
2024-03-05 13:37:02 +0100

    primer cambio

--- src/films.js
+++ src/films.js
@@ -1,18 +1,25 @@
+const movies = require('../src/data');
+
 // Exercise 1: Get the array of all directors.
-function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+function getAllDirectors(movies) {
+  let result = movies.map(peli=> (peli.director));
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {
- 
+function getMoviesFromDirector(movies, director) {
+  let result = movies.filter(peli=> peli.director === director);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {
-  
+function moviesAverageOfDirector(movies, director) {
+  let result = movies.filter(peli=> peli.director === director)
+  let puntuacionTotal = result.reduce((total,peli) => total + peli.score,0);
+  let puntuacionMedia = result.length > 0 ? puntuacionTotal / result.length : 0;
+  let mediaRedondeada = parseFloat(puntuacionMedia.toFixed(2));
+  console.log("EXERCICE 3 ->", mediaRedondeada);
+  return mediaRedondeada ;
 }
 
 // Exercise 4:  Alphabetic order by title 
